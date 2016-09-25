9f01cd27d80ac7684ce21265ce1723d3e2141623
Andriy Rysin <arysin@gmail.com>
2016-09-25 11:51:32 -0400

    more swagger changes

--- grails-app/controllers/languagetool_nlp/TokenizeController.groovy
+++ grails-app/controllers/languagetool_nlp/TokenizeController.groovy
@@ -4,27 +4,44 @@ package languagetool_nlp
 import grails.rest.*
 import grails.converters.*
 
-import io.swagger.annotations.*
+//import io.swagger.annotations.*
+import com.wordnik.swagger.annotations.*
 
 
-@Api(value = "Tokenization services", description = "Tokenization services for Ukrainian language")
+@Api(value = "Tokenization services", 
+    description = "Tokenization services for Ukrainian language",
+    produces = 'application/json',
+    consumes = 'application/json'
+)
 class TokenizeController {
     static responseFormats = ['json']
     static allowedMethods = [save: "POST"]
+    static defaultAction = "save"
 
     def tokenizeService
 
 
-    @ApiOperation(value = "Tokenizes the text into sentences and then into words")
-    def save() {
-        if( ! request.JSON.body?.text ) {
+    @ApiOperation(value = "Tokenizes the text into sentences and then into words", 
+                httpMethod = "POST"
+                ,
+                response = TokenizedText.class)
+    @ApiResponses([
+        @ApiResponse(code = 400, message = "Invalid body provided")
+    ])
+    @ApiImplicitParams([
+        @ApiImplicitParam(name = 'body', paramType = 'body', required = true, dataType='InputBody', 
+            value='Body text; e.g<br>{"text": "І.А. Іванов прийшов додому. І з\'їв 2 тис. кавунів."}')
+    ])
+    def save(
+    ) {
+        if( ! request.JSON?.text ) {
             render(status: 400, text: "body.text not specified: " + request.JSON)
             return
         }
 
         try {
-            def tokens = tokenizeService.tokenize(request.JSON.body, params)
-            render tokens: tokens
+            def tokens = tokenizeService.tokenize(request.JSON, params)
+            render new TokenizedText(tokens: tokens) as JSON
         }
         catch(Exception e) {
             e.printStackTrace()
