5167c5cfafe51efe7c403e1e1178508a06b0c4c2
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-01-11 14:43:02 +0200

    lose swagger: could not get it to compile

--- grails-app/conf/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-// Place your Spring DSL code here
-import languagetool_nlp.ApiDocumentationConfiguration
-
- // Place your Spring DSL code here
-beans = {
-    apiDocumentationConfiguration(ApiDocumentationConfiguration) {
-    }
-}
